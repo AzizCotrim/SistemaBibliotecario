96607365329097a5eb2a14139a3f7133b1925852
Aziz Cotrim <azizcotrim8@gmail.com>
2026-01-07 18:04:55 -0300

    Mudancas na CategoriaRepository

--- Sistema de Biblioteca/Infrastructure/Repositories/CategoriaRepository.cs
+++ Sistema de Biblioteca/Infrastructure/Repositories/CategoriaRepository.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.Data.SqlClient;
 using Sistema_de_Biblioteca.Domain.Entities.Categoria;
 using Sistema_de_Biblioteca.Infrastructure.Database;
+using System.Data;
 
 namespace Sistema_de_Biblioteca.Infrastructure.Repositories
 {
     internal class CategoriaRepository
     {
         private readonly DataBase _db = new DataBase();
+
+        public bool ExisteCategoria(string nome)
+        {
+            using (SqlConnection con = _db.GetSqlConnection()) {
+                string sql = @"SELECT COUNT(*)
+                                 FROM BI_CATEGORIAS
+                                WHERE CAT_NOME LIKE @nome";
+
+                using(SqlCommand cmd = new SqlCommand(sql, con)) {
+                    cmd.Parameters.AddWithValue("@nome", nome);
+
+                    int qtd = (int)cmd.ExecuteScalar();
+
+                    return qtd > 0;
+                }
+            }
+        }
 
         public List<Categoria> BuscarCategoriasSimples()
         {
@@ -28,6 +46,30 @@ namespace Sistema_de_Biblioteca.Infrastructure.Repositories
                 }
             }
 
+            return list;
+        }
+        //Table-Valued Parameter (TVP)
+        public List<Categoria> BuscarCategoriaFiltro(List<int> IdCategoria)
+        {
+            List<Categoria> list = new List<Categoria>();
+
+            using (SqlConnection con = _db.GetSqlConnection()) {
+                string sql = @"SELECT CAT_ID, CAT_NOME
+                                 FROM BI_CATEGORIAS
+                                WHERE CAT_ID IN (@IdCategoria)";
+
+                using (SqlCommand cmd = new SqlCommand(sql,con)) {
+                    cmd.Parameters.Add("@IdCategoria", SqlDbType.Int).Value = IdCategoria;
+                    using (SqlDataReader dr = cmd.ExecuteReader()) {
+                        while (dr.Read()) {
+                            int id = dr.GetInt32(0);
+                            string nome = dr.GetString(1);
+
+                            list.Add(new Categoria(id, nome));
+                        }
+                    }
+                }
+            }
             return list;
         }
     }
